529f7632784cbaebef5a4f2704299e552df88373
Knolle-03 <Knolle-03@users.noreply.github.com>
2019-04-12 16:14:39 +0200

    added several "Denksportaufgaben"

---
+++ src/de/hawh/ld/denksport/Shorthand.java
@@ -0,0 +1,13 @@
+package de.hawh.ld.denksport;
+
+public class Shorthand {
+    public static void main(String[] args) {
+        byte x = 48;
+        short i = 9389;
+
+        x += i;
+        System.out.println(x);
+        //x = x + i;
+    }
+
+}
